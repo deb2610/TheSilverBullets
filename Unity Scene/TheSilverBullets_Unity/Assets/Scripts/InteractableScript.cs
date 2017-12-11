b7ca9f860c44f41cc91066401bfc343383872b32
MAClavell <michaelclavell11@gmail.com>
2017-12-11 14:18:06 -0500

    Interactability. Still have to do an outline shader

---
+++ Unity Scene/TheSilverBullets_Unity/Assets/Scripts/InteractableScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableScript : MonoBehaviour {
+
+	public AnimationClip anim;
+	Animator animator;
+	public Material outlineMat;
+	public Material defaultMat;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlayAnimation()
+	{
+		animator.Play(anim.name);
+	}
+
+	void OnMouseDown()
+	{
+		PlayAnimation();
+	}
+
+	private void OnMouseEnter()
+	{
+		GetComponent<Renderer>().material = outlineMat;
+	}
+
+	private void OnMouseExit()
+	{
+		GetComponent<Renderer>().material = defaultMat;
+	}
+}
